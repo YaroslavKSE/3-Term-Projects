ce084ee24cf45965d48b5871b9b56ae9e4ae32bd
Maxim Pozdniakov <94074148+qualia4@users.noreply.github.com>
2022-01-17 20:47:48 +0200

    Update Stack.cs

--- FirstTask/Stack.cs
+++ FirstTask/Stack.cs
@@ -1,44 +1,37 @@
-﻿using System;
-
 namespace FirstTask
 {
+    using System;
+
+    
     public class Stack
     {
-        private string[] _array = new string[50];
+        private const int Capacity = 50;
 
-        private int _pointer = 0;
+        private string[] _array = new string[Capacity];
 
-        public void Push(string element)
+        private int _pointer;
+
+        public void Push(string value)
         {
             if (_pointer == _array.Length)
-                throw new Exception("Stack overflow");
-            var ExtendedArray = new string[_array.Length * 2];
-            for (var i = 0; i < _array.Length; i++) ExtendedArray[i] = _array[i];
+            {
+                throw new Exception("Stack overflowed");
+            }
 
-            _array = ExtendedArray;
-            _array[_pointer] = element;
-            _pointer += 1;
+            _array[_pointer] = value;
+            _pointer++;
         }
 
-        public string Pop()
+        public string Pull()
         {
             if (_pointer == 0)
             {
-                throw new Exception("Stack is already empty");
+                return null;
             }
-            var returnValue = _array[_pointer - 1];
-            _pointer -= 1;
-            _array[_pointer] = null;
-            return returnValue;
-        }
 
-        public string Seek()
-        {
-            if (_pointer == 0)
-            {
-                throw new Exception("Stack is already empty");
-            }
-            return _array[_pointer - 1];
+            var value = _array[_pointer];
+            _pointer--;
+            return value;
         }
     }
 }
