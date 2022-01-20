500fa11706e11858883073c8784c22562abf3a99
Maxim Pozdniakov <94074148+qualia4@users.noreply.github.com>
2022-01-21 00:53:00 +0200

    Add files via upload

---
+++ FirstTask/Program.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Linq;
+using Stack = FirstTask.Stack;
+using Queue = FirstTask.Queue;
+
+
+var input = Console.ReadLine();
+var tokens = Token(input);
+Console.WriteLine(tokens.ToString());
+var postfixTokens = InfixToPostfix(tokens);
+Console.WriteLine(postfixTokens.ToString());
+var result = Calculate(postfixTokens);
+Console.WriteLine(result);
+
+Queue Token(string expression)
+{
+    var tokenized = new Queue();
+    var b = new Queue();
+    for (int i = 0; i < expression.Length; i++)
+    {
+        if (char.IsDigit(expression[i]))
+        {
+            b.Enqueue(expression[i].ToString());
+        }
+        else if (expression[i] == ' ')
+        {
+            if (!b.IsEmpty())
+                tokenized.Enqueue(combineNumber(b));
+        }
+        else if (expression[i] == '(')
+        {
+            tokenized.Enqueue(expression[i].ToString());
+        }
+        else
+        {
+            if (!b.IsEmpty())
+                tokenized.Enqueue(combineNumber(b));
+            tokenized.Enqueue(expression[i].ToString());
+        }
+        
+    }
+    if (!b.IsEmpty())
+        tokenized.Enqueue(combineNumber(b));
+    return tokenized;
+}
+
+Queue InfixToPostfix(Queue tokenized)
+{
+    var operatorStack = new Stack();
+    var output = new Queue();
+    while (!tokenized.IsEmpty())
+    {
+        var token = tokenized.Dequeue();
+        if (int.TryParse(token, out _))
+        {
+            output.Enqueue(token);
+        }
+        else if(isOperator(token))
+        {
+            while (!operatorStack.IsEmpty() && checkPriority(operatorStack.GetLast()) > checkPriority(token) || 
+                   checkPriority(operatorStack.GetLast()) == checkPriority(token) && token != "^")
+            {
+                if (operatorStack.GetLast() != "(")
+                    output.Enqueue(operatorStack.Pull());
+                else break;
+            }
+            operatorStack.Push(token);
+        }
+        else if (token == "(")
+        {
+            operatorStack.Push(token);
+        }
+        else if (token == ")")
+        {
+            while (operatorStack.GetLast() != "(")
+            {
+                output.Enqueue(operatorStack.Pull());
+            }
+
+            operatorStack.Pull();
+        }
+    }
+
+    while (!operatorStack.IsEmpty())
+    {
+        output.Enqueue(operatorStack.Pull());
+    }
+    return output;
+}
+
+
+String Calculate(Queue postfixTokenList)
+{
+    var buffer = new Stack();
+    while (!postfixTokenList.IsEmpty())
+    {
+        var token = postfixTokenList.Dequeue();
+        if (int.TryParse(token, out int _))
+        {
+            buffer.Push(token);
+        }
+        else
+        {
+            var secondNum = buffer.Pull();
+            var firstNum = buffer.Pull();
+            buffer.Push(counter(firstNum, secondNum, token));
+        }
+    }
+    var result = buffer.Pull();
+    return result;
+}
+
+
+int checkPriority(string operation)
+{
+    if (operation == "+" || operation == "-")
+    {
+        return 0;
+    }
+    else if (operation == "*" || operation == "/")
+    {
+        return 1;
+    }
+    else return 2;
+}
+
+bool isOperator(String oper)
+{
+    if (oper == "+" || oper == "-" || oper == "/" || oper == "*" || oper == "^")
+        return true;
+    return false;
+}
+
+
+string combineNumber(Queue queue)
+{
+    string result = "";
+    while (!queue.IsEmpty())
+    {
+        result = result + queue.Dequeue();
+    }
+    return result;
+}
+
+string counter(string firstNum, string secondNum, string oper)
+{
+    double result = 0;
+    switch (oper)
+    {
+        case "+":
+            result = double.Parse(firstNum) + double.Parse(secondNum);
+            break;
+        case "-":
+            result = double.Parse(firstNum) - double.Parse(secondNum);
+            break;
+        case "/":
+            result = double.Parse(firstNum) / double.Parse(secondNum);
+            break;
+        case "*":
+            result = double.Parse(firstNum) * double.Parse(secondNum);
+            break;
+        case "^":
+            result = Math.Pow(double.Parse(firstNum), double.Parse(secondNum));
+            break;
+        default:
+            throw new Exception("Illegal operation");
+    }
+    return result.ToString();
+}
